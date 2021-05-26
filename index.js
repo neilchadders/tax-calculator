bf51e465169503bbb20934ce883a8cba2a46ff7f
Neil Chadwick <neilchadders@hotmail.com>
2021-05-26 15:05:56 +0100

    adding DOM variables

---
+++ index.js
@@ -0,0 +1,5 @@
+
+	let salaryInput = document.getElementById("salary-input").value;
+	let taxDue = document.getElementById("tax-due");
+	let natIns = document.getElementById("nat-ins");
+	let netPay = document.getElementById("net-pay");
