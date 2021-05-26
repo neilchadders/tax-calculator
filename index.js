225f4f6d7f68b4c2574ce3ee56af7fa7f5016f8e
Neil Chadwick <neilchadders@hotmail.com>
2021-05-26 15:38:42 +0100

    refactoring code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 
 const calculateButton = document.getElementById("btn");
-
+const base = 15000;
+const higherThresh = 50000;
 
 
 function salaryCalculator(){
@@ -12,18 +13,18 @@ function salaryCalculator(){
 	let taxDue = document.getElementById("tax-due");
 	let natIns = document.getElementById("nat-ins");
 	let netPay = document.getElementById("net-pay");
-	let salaryNumber = parseFloat(salaryInput);
+	let salaryNumber = parseFloat(salaryInput); /* ensuring that the salaryInput variable converts to number*/
 
 	let higherRateTax = 7000;
 	let higherRateNi = 4200;
 	
 	
 		/* Tax and NI for salary up to 15000*/
-	if (salaryNumber <= 15000){
+	if (salaryNumber <= base){
 		netPay.innerHTML = salaryInput;
 		/* Tax and NI for salary of 15000 - 50000*/
-	} else if ( salaryInput > 15000 && salaryInput <= 50000){
-		let difference =  salaryNumber - 15000;
+	} else if ( salaryInput > base && salaryInput <= higherThresh){
+		let difference =  salaryNumber - base;
 		let tax = difference * 0.2;
 		let ni = difference * 0.12;
 		let net = salaryNumber - (tax + ni); 
@@ -33,8 +34,8 @@ function salaryCalculator(){
 		netPay.innerHTML = net;
 
 		/* Tax and NI for salary of over 50000 */
-	} else if (salaryNumber > 50000){
-		let highDifference = salaryNumber - 50000;
+	} else if (salaryNumber > higherThresh){
+		let highDifference = salaryNumber - higherThresh;
 		let highTax = higherRateTax + (highDifference * 0.4);
 		let highNi = higherRateNi + (highDifference * 0.02);
 		let highNet = salaryNumber - (highTax + highNi);
